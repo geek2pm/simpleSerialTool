db8f7e717b34c986ad0e04533bc2efd1c042d1e0
geek2pm <geek2pm@163.com>
2020-01-18 11:02:23 -0500

    check serial state per second

--- app.py
+++ app.py
@@ -6,11 +6,17 @@ import sys,time
 import serial
 import serial.tools.list_ports
 
+TITLE = 'simple Serial Tool'
+
 qtCreatorFile = "ui/main.ui" # Enter file here.
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
 qtCreatorFile = "ui/message.ui" # Enter file here.
 Ui_Dialog, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+def center_on_screen(self,QtWidgets):
+    resolution = QtWidgets.QDesktopWidget().screenGeometry()
+    self.move((resolution.width() / 2) - (self.frameSize().width() / 2),(resolution.height() / 2) - (self.frameSize().height() / 2))
 
 class DataCaptureThread(QtCore.QThread):
     data_ok = QtCore.pyqtSignal(object)
@@ -31,31 +37,44 @@ class DataCaptureThread(QtCore.QThread):
         time.sleep(0.1)
         self.run()
 
+class GetCOMsThread(QtCore.QThread):
+    data = QtCore.pyqtSignal(object)
+    last = None
+    def __init__(self):
+        QtCore.QThread.__init__(self)
+    def run(self):
+        ports = serial.tools.list_ports.comports()
+        if ports != self.last:
+            self.data.emit(ports)
+            self.last = ports
+        # QtCore.QThread.sleep(1)
+        time.sleep(1)
+        self.run()
+
 class Message(QtWidgets.QMainWindow, Ui_Dialog):
     def __init__(self, parent=None):
         super(Message, self).__init__(parent)
         self.setupUi(self)
-        resolution = QtWidgets.QDesktopWidget().screenGeometry()
-        self.move((resolution.width() / 2) - (self.frameSize().width() / 2),
-                  (resolution.height() / 2) - (self.frameSize().height() / 2))
+        center_on_screen(self,QtWidgets)
 
 class App(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(App, self).__init__(parent)
         self.setupUi(self)
-        resolution = QtWidgets.QDesktopWidget().screenGeometry()
-        self.move((resolution.width() / 2) - (self.frameSize().width() / 2),
-                  (resolution.height() / 2) - (self.frameSize().height() / 2))
+        center_on_screen(self,QtWidgets)
         self.btn_open.clicked.connect(self.btn_open_Clicked)
         self.btn_send.clicked.connect(self.btn_send_Clicked)
         self.btn_clear.clicked.connect(self.btn_clear_Clicked)
+        self.btn_about.clicked.connect(self.btn_about_Clicked)
         self.getCOMs()
 
     def btn_open_Clicked(self):
 
-        if self.btn_open.text() == 'opened':
-            self.btn_open.setText('closed')
+        if self.btn_open.text() == 'close':
+            self.btn_open.setText('open')
+            self.setWindowTitle("{} ({})".format(TITLE,"closed"))
             self.ser=None
+            self.btn_send.setEnabled(False)
         else:
             com = self.comboBox_COMs.currentData(self.comboBox_COMs.currentIndex())
             baud = self.comboBox_baud.currentData(self.comboBox_baud.currentIndex())
@@ -70,10 +89,12 @@ class App(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.dialog.show()
             else:
                 print('ok')
-                self.btn_open.setText('opened')
+                self.btn_open.setText('close')
+                self.setWindowTitle("{} ({})".format(TITLE,"opened"))
                 self.dataCollectionThread = DataCaptureThread(self.ser)
                 self.dataCollectionThread.data_ok.connect(self.on_data_ready)
                 self.dataCollectionThread.start()
+                self.btn_send.setEnabled(True)
     def on_data_ready(self, data):
         # self.textBrowser.append("{}\n".format(data))
         self.textBrowser.append(data)
@@ -85,6 +106,8 @@ class App(QtWidgets.QMainWindow, Ui_MainWindow):
             self.dialog = Message(self)
             self.dialog.textBrowser.setText("")
             self.dialog.textBrowser.append("<h2>error</h2>")
+            self.dialog.textBrowser.setOpenLinks(True)
+            self.dialog.textBrowser.setOpenExternalLinks(True)
             self.dialog.textBrowser.append(str(err))
             self.dialog.show()
         else:
@@ -92,13 +115,37 @@ class App(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def btn_clear_Clicked(self):
         self.textBrowser.setText("")
+    
+    def btn_about_Clicked(self):
+        self.dialog = Message(self)
+        self.dialog.textBrowser.setText("")
+        self.dialog.textBrowser.append("<h2>about</h2>")
+        link = "<a target='_blank' href='https://github.com/geek2pm/simpleSerialTool'>https://github.com/geek2pm/simpleSerialTool</a>"
+        self.dialog.textBrowser.append(link)
+        self.dialog.textBrowser.setOpenExternalLinks(True)
+        self.dialog.show()
 
     def getCOMs(self):
-        ports = serial.tools.list_ports.comports()
-        for port, desc, hwid in sorted(ports):
-            print("{}: {} [{}]".format(port, desc, hwid))
-            self.comboBox_COMs.addItem(port)
-
+        self.getCOMsThread = GetCOMsThread()
+        self.getCOMsThread.data.connect(self.on_data_coms)
+        self.getCOMsThread.start()
+    
+    def on_data_coms(self, ports):
+        if len(ports)>0:
+            self.btn_open.setEnabled(True)
+            self.btn_clear.setEnabled(True)
+            self.comboBox_COMs.clear()
+            for port, desc, hwid in sorted(ports):
+                print("{}: {} [{}]".format(port, desc, hwid))
+                self.comboBox_COMs.addItem(port)
+            self.btn_open.setText('open')
+            self.setWindowTitle("{} ({})".format(TITLE,"ready to open"))
+        else:
+            self.btn_open.setEnabled(False)
+            self.btn_send.setEnabled(False)
+            self.btn_clear.setEnabled(False)
+            self.btn_open.setText('open')
+            self.setWindowTitle("{} ({})".format(TITLE,"no device found"))
 def main():
     app = QApplication(sys.argv)
     form = App()
